6bc20b3754a98dea4be093c976133775a3dc29d1
miloneko <satohannn@gmail.com>
2022-05-26 18:24:49 +0900

    Add: fetchボタンでイベント

--- frontend/src/components/Calendar.vue
+++ frontend/src/components/Calendar.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
     <h1>Calendar</h1>
-    <p>events:</p>
-    <p>{{ events }}</p>
+    <ul>
+        <li v-for="event in events" :key="event.id">
+          {{ event.name }}
+        </li>
+      </ul>
     <button type="submit" @click="fetchEvents()">fetchEvents</button>
+
+    <CalendarDetails />
   </div>
 </template>
 
 <script>
 import axios from 'axios';  // axiosをインポートする
+import CalendarDetails from './CalendarDetails.vue';
 
 export default {
   name: 'Calendar',
+  components: {
+     CalendarDetails,
+   },
   data: () => ({
     events: []
   }),
