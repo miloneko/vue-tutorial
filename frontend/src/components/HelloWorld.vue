806cb0b58816d7da71d417813a095bfe67fbf5e3
miloneko <satohannn@gmail.com>
2022-05-26 18:16:16 +0900

    Add:cros

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,9 +1,18 @@
 <template>
-  <p>Hello World!</p>
+  <div>
+    <p>Hello World!</p>
+    <p>
+      Hello World! Hello World! Hello World! Hello World! Hello World! Hello
+      World!
+    </p>
+  </div>
 </template>
 
 <script>
 export default {
   name: "HelloWorld",
+  data: () => ({
+    message: "Hello World!",
+  }),
 };
 </script>
